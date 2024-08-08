ae273c27f9503a4ea34d83deb1c36ae5729a9a74
ireone01 <huyvip10a3@gmail.com>
2024-08-08 17:30:58 +0700

    Fetch and display data detailScreen

---
+++ app/src/main/java/com/example/sun/utils/ForecastDayEntry.kt
@@ -0,0 +1,10 @@
+package com.example.sun.utils
+
+object ForecastDayEntry {
+    const val DATE = "date"
+    const val AVG_TEMP = "avgtemp_c"
+    const val MAX_TEMP = "maxtemp_c"
+    const val MIN_TEMP = "mintemp_c"
+    const val CONDITION = "condition"
+    const val ICON = "icon"
+}
